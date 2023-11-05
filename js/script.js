cea4f4dafdd30a1c6b80028d4876a397eec9d3ae
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-11-05 21:21:42 +0600

    bootstrap button with popover js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,25 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-video', {
         videoId: 'YKJyP8L6QEs',
         playerVars: {
-            'controls': 1,
-            'rel': 0,
-            'showinfo': 0
+            'controls': 1
         },
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let subscribeButton = document.getElementById('subscribeButton');
+
+    if (subscribeButton) {
+        subscribeButton.addEventListener('click', function () {
+            let popover = new bootstrap.Popover(subscribeButton, {
+                content: "You have subscribed!",
+                trigger: "manual",  // Ensure manual trigger
+            });
+            popover.show();
+
+            setTimeout(function() {
+                popover.hide();
+            }, 4000);
+        });
+    }
+});
